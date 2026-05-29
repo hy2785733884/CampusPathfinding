c6a9b885bba276388e8a8df037b0c35f9eab8afc
hy2785733884 <hy2785733884@gmail.com>
2026-05-29 20:08:16 +0800

    支持多个文件并且可以在运行时选择

--- main.cpp
+++ main.cpp
@@ -8,13 +8,64 @@
 #include <chrono>
 #include <conio.h>
 #include <easyx.h>
+#include <io.h>       
 using namespace std;
 
-int main() {
-    // 1. 读取地图文件
-    ifstream file("map.txt");
+int main(int argc, char* argv[]) {
+    string filepath;
+    if (argc >= 2) {
+        // 方式A：命令行参数指定
+        filepath = argv[1];
+    }
+    else {
+        // 方式B：扫描 maps 文件夹列出所有.txt
+        vector<string> mapFiles;
+        string mapsDir = "maps";
+
+        string searchPattern = mapsDir + "\\*.txt";
+        _finddata_t fileInfo;
+        intptr_t handle = _findfirst(searchPattern.c_str(), &fileInfo);
+
+        if (handle != -1) {
+            do {
+ 
+                if (!(fileInfo.attrib & _A_SUBDIR)) {
+                    mapFiles.push_back(fileInfo.name);
+                }
+            } while (_findnext(handle, &fileInfo) == 0);
+            _findclose(handle);
+        }
+
+        if (mapFiles.empty()) {
+            cerr << "maps 文件夹中没有找到 .txt 地图文件！" << endl;
+            return 1;
+        }
+
+        cout << "可用的地图文件：" << endl;
+        for (size_t i = 0; i < mapFiles.size(); i++) {
+            cout << "  " << (i + 1) << ". " << mapFiles[i] << endl;
+        }
+
+        cout << "请输入编号选择地图（默认 1）：";
+        string input;
+        getline(cin, input);
+
+        int choice = 1;
+        if (!input.empty()) {
+            choice = stoi(input);
+        }
+        if (choice < 1 || choice >(int)mapFiles.size()) {
+            choice = 1;
+        }
+
+        filepath = mapsDir + "/" + mapFiles[choice - 1];
+        cout << "已选择：" << filepath << endl;
+    }
+
+
+    ifstream file(filepath);
     if (!file.is_open()) {
-        cerr << "无法打开 map.txt 文件！请确认该文件在 exe 同级目录下。" << endl;
+        cerr << "无法打开地图文件：" << filepath << endl;
         return 1;
     }
 
@@ -45,7 +96,7 @@ int main() {
         }
     }
 
-    // 2. A* 寻路 + 高精度计时
+ 
     int aNodes = 0, jNodes = 0;
     vector<pair<int, int>> aExplored, jExplored;
 
@@ -55,14 +106,14 @@ int main() {
     chrono::duration<double, milli> aTimeSpan = t2 - t1;
     double aTime = aTimeSpan.count();
 
-    // 3. JPS 寻路 + 高精度计时
+  
     auto t3 = chrono::high_resolution_clock::now();
     auto jPath = jps(startX, startY, goalX, goalY, jNodes, jExplored);
     auto t4 = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> jTimeSpan = t4 - t3;
     double jTime = jTimeSpan.count();
 
-    // 4. 可视化展示
+    
     drawMap(aPath, jPath, aTime, jTime, aNodes, jNodes, aExplored, jExplored);
 
     _getch();
